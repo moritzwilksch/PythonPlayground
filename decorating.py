a657b5cf2b3fd344101c6d29228c55dc1eba3a42
Moritz <moritzwilksch@gmail.com>
2020-01-04 15:41:15 +0100

    Added Decorators in python file

---
+++ decorating.py
@@ -0,0 +1,72 @@
+# %% [markdown]
+""" 
+# Example of Decorating Python Functions
+## Definition of Functions
+"""
+# %%
+def func_output_to_upper(func):
+    def wrapper():
+        result = func()
+        return result.upper()
+    return wrapper
+
+def output_text():
+    return "Lorem Ipsum blabla"
+
+# %% [markdown]
+# ## Decorating - the verbose way
+# %%
+# Normal way of decorating a function
+test = func_output_to_upper(output_text)
+print(test())
+
+# %% [markdown]
+# # Decorating - the easy way
+# %%
+# Decorator-way of decorating a function
+@func_output_to_upper
+def output_text_2():
+    return "Second Lorem Ipsum blabla for real decoration" 
+
+print(output_text_2())
+
+# %% [markdown]
+# ## Decorating *Functions* with Arguments
+# %%
+def print_call(func):
+    def wrapper(arg1, arg2):
+        print(f"{func.__name__}({arg1}, {arg2}) was called.")
+        res = func(arg1, arg2)
+        print("result =", res)
+    return wrapper
+
+@print_call
+def multiply(a, b):
+    return a*b
+
+print(multiply(12,11))
+
+# %% [markdown]
+# ## *Decorators* with Arguments
+# %%
+def change_output_case(change_to_upper):
+    def change_case(func):
+        def wrapper():
+            res = func()
+            if change_to_upper:
+                res = res.upper()
+            else:
+                res = res.lower()
+            return res
+        return wrapper
+    return change_case
+
+@change_output_case(change_to_upper=False)
+def talk():
+    return "This is a Text with MEssed-up Casing"
+print(talk())
+
+@change_output_case(change_to_upper=True)
+def talk():
+    return "This is a Text with MEssed-up Casing"
+print(talk())
