90cd09fdcabfc0c95408bbda52539aea6f70bc15
Moritz <moritzwilksch@gmail.com>
2020-01-13 17:34:19 +0100

    Trying out json normalize and seaborn options

---
+++ fancyPlotting.py
@@ -0,0 +1,19 @@
+# %%
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# %%
+df = pd.read_csv("./data/WIKI-AAPL.csv").drop(["Ex-Dividend", "Open", "High", "Low", "Close", "Split Ratio", "Volume"], axis=1)
+df["Date"] = df["Date"].astype("datetime64")
+df.columns = ["date", "open", "high", "low", "close", "volume"]
+df[["open", "high", "low", "close"]] = df[["open", "high", "low", "close"]].astype("float16")
+df["volume"] = df["volume"].astype("int32")
+df = df.loc[df.date.dt.year >= 2005]
+df.head()
+df.info()
+
+# %%
+sns.set_style("ticks", rc={"figure.frameon": False, "font.family": "serif", "axes.spines.top": False, "axes.spines.right": False, "lines.linewidth": 0.9})
+plt.
+sns.lineplot(data=df, x="date", y="close")
