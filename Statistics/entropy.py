d53ee6340166e46e3c504fce76c296441773c8c1
Moritz <moritzwilksch@gmail.com>
2020-02-11 11:07:09 +0100

    minor change

--- Statistics/entropy.py
+++ Statistics/entropy.py
@@ -4,9 +4,13 @@ def calc_entropy(s):
     letter_counts = {letter: s.count(letter) for letter in s}
     l = len(s)
     probability_dict = {letter: letter_counts[letter]/l for letter in letter_counts}
-    return {letter: - np.sum(probability_dict[letter] * np.log2(probability_dict[letter])) for letter in probability_dict}
+    return sum([- np.sum(probability_dict[letter] * np.log2(probability_dict[letter])) for letter in probability_dict])
 
 # %%
 
+
+print(calc_entropy("0000000000000"))
+print(calc_entropy("0000000100000"))
 print(calc_entropy("0000010100000"))
 print(calc_entropy("1010111010110"))
+print(calc_entropy("1111111111111"))
