295abfed84f0826769dc2fa0ba1206da58a527cc
Moritz <moritzwilksch@gmail.com>
2020-03-02 13:41:50 +0100

    added pandas pipelining

--- sklearn-pipelining.py
+++ sklearn-pipelining.py
@@ -1,4 +1,7 @@
 # %%
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.pipeline import Pipeline
+from customFunctions.functionDefinitions import *
 import pandas as pd
 import seaborn as sns
 import sklearn
@@ -9,9 +12,6 @@ df = pd.read_csv("./data/titanic.csv")
 df.info()
 
 # %%
-from customFunctions.functionDefinitions import *
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import FunctionTransformer
 
 # Defining columns
 category_columns = ["Survived", "Pclass", "Sex", "Embarked"]
@@ -19,14 +19,24 @@ useless_cols = ["PassengerId", "Ticket", "Cabin"]
 
 # Pipeline
 pl1 = Pipeline(steps=[
-    ("1", FunctionTransformer(categorize, validate=False, kw_args={"category_columns": category_columns})),
-    ("2", FunctionTransformer(drop_useless_cols, validate=False, kw_args={"useless_cols": useless_cols})),
+    ("1", FunctionTransformer(categorize, validate=False,
+                              kw_args={"category_columns": category_columns})),
+    ("2", FunctionTransformer(drop_useless_cols,
+                              validate=False, kw_args={"useless_cols": useless_cols})),
     ("3", FunctionTransformer(age_imputer, validate=False)),
-    ("4", FunctionTransformer(log_transform, validate=False, kw_args={"col_to_transform": "Fare"}))
+    ("4", FunctionTransformer(log_transform,
+                              validate=False, kw_args={"col_to_transform": "Fare"}))
 ])
 
 # Fit-transform pipeline
 df = pl1.fit_transform(df)
+
+# %%
+# Pipelining with pandas
+(df .pipe(categorize, category_columns=category_columns)
+    .pipe(drop_useless_cols, useless_cols=useless_cols)
+    .pipe(age_imputer)
+    .pipe(log_transform, col_to_transform="Fare"))
 
 # %%
 # Save file
