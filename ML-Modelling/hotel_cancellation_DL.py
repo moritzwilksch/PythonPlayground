b60c894e9b11a3c23e9794d1a76f9e0678e689ed
Moritz Wilksch <moritzwilksch@gmail.com>
2022-03-27 17:14:38 +0200

    neural net scaffolding

--- ML-Modelling/hotel_cancellation_DL.py
+++ ML-Modelling/hotel_cancellation_DL.py
@@ -57,12 +57,9 @@ categoricals = [
 ]
 
 
-# for catcol in categoricals:
-#     X_train_original[catcol] = X_train_original[catcol].astype("category")
-#     X_train_original[catcol] = X_train_original[catcol].cat.add_categories(["missing"])
-#     X_train_original[catcol] = X_train_original[catcol].fillna("missing")
-#     X_train_original[catcol] = X_train_original[catcol].cat.codes
 X_train_original[categoricals] = X_train_original[categoricals].astype("category")
+for catcol in categoricals:
+    X_train_original[catcol] = X_train_original[catcol].cat.codes
 
 
 #%%
@@ -79,7 +76,7 @@ df = df.with_columns(
             ["arrival_date_year", "arrival_date_month", "arrival_date_day_of_month"],
             sep="-",
         )
-        .str.strptime(pl.Date, "%Y-%B-%d")
+        .str.strptime(pl.Date, "%Y-%m-%d", strict=False)
         .dt.weekday()
         .alias("arrival_weekday"),
         # total stay
@@ -120,4 +117,99 @@ xtrain, xval, ytrain, yval = train_test_split(
     df.drop(cols_to_drop, axis=1), y_train, test_size=0.2, random_state=42
 )
 
+# # LGBM max val AUC 0.88 (accuracy: 0.89)
+
+# #%%
+# from sklearn.impute import KNNImputer, SimpleImputer
+
+# imputer = SimpleImputer()
+# numcols = xtrain.select_dtypes(exclude="category").columns
+# xtrain[numcols] = pd.DataFrame(imputer.fit_transform(xtrain[numcols]), columns=numcols)
+# xval[numcols] = pd.DataFrame(imputer.fit_transform(xval[numcols]), columns=numcols)
+
+# #%%
+# from lightgbm import LGBMClassifier
+
+# # clf = LGBMClassifier(n_estimators=10_000)
+# # clf.fit(
+# #     xtrain,
+# #     ytrain,
+# #     eval_metric=["auc", "binary_logloss"],
+# #     eval_set=(xval, yval),
+# #     # categorical_feature=categoricals,
+# #     early_stopping_rounds=500,
+# # )
+
+# from sklearn.ensemble import RandomForestClassifier
+
+# clf = RandomForestClassifier(n_jobs=-1, max_depth=10)
+# clf.fit(xtrain, ytrain)
+
+# #%%
+# from sklearn.metrics import classification_report, roc_auc_score
+
+# print(classification_report(ytrain, clf.predict(xtrain)))
+# print(classification_report(yval, clf.predict(xval)))
+# print(roc_auc_score(yval, clf.predict(xval)))
+
+# #%%
+# from imblearn.over_sampling import SMOTE, SMOTENC
+
+# smote = SMOTENC(
+#     n_jobs=-1, categorical_features=[c in categoricals for c in xtrain.columns]
+# )
+# xtrain_smote, ytrain_smote = smote.fit_resample(xtrain, ytrain)
+# #%%
+# import optuna
+
+
+# def objective(trial: optuna.Trial):
+#     params = {
+#         "max_depth": trial.suggest_int("max_depth", 2, 100),
+#         "min_samples_split": trial.suggest_int("min_samples_split", 2, 10),
+#         "min_samples_leaf": trial.suggest_int("min_samples_leaf", 1, 50),
+#         "max_features": trial.suggest_float("max_features", 0.1, 1),
+#     }
+
+#     clf = RandomForestClassifier(n_jobs=-1, **params)
+#     clf.fit(xtrain_smote, ytrain_smote)
+#     return roc_auc_score(yval, clf.predict(xval))
+
+
+# study = optuna.create_study(
+#     storage="sqlite:///optuna_study.db",
+#     direction="maximize",
+#     study_name="moritz_rf",
+#     load_if_exists=True,
+# )
+
+# study.optimize(objective, n_trials=100)
+
 #%%
+import tensorflow as tf
+
+catcols_embedding = {
+    "hotel": 2,
+    "arrival_date_month": 5,
+    "meal": 2,
+    "country": 10,
+    "market_segment": 5,
+    "distribution_channel": 2,
+    "reserved_room_type": 2,
+    "assigned_room_type": 2,
+    "deposit_type": 2,
+    "customer_type": 2,
+}
+catcols_cardinalities = df[categoricals].nunique().to_dict()
+
+
+class HotelModel(tf.keras.Model):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.embeddings = {
+            col: tf.keras.layers.Embedding(
+                catcols_cardinalities.get(col) + 1, catcols_embedding.get(col)
+            )
+            for col in categoricals
+        }
