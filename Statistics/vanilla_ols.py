3783c2a97abff4318a08a97c5fdf55740628b77d
Moritz Wilksch <moritzwilksch@gmail.com>
2022-08-17 12:04:58 +0000

    diff in means vs. mean of diffs

--- Statistics/vanilla_ols.py
+++ Statistics/vanilla_ols.py
@@ -28,4 +28,21 @@ from scipy import stats
 fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
 rvs = stats.expon(scale=1).rvs(size=1000)
 axes[0].hist(rvs)
-axes[1].hist()
+
+
+#%%
+a = stats.norm(loc=0.2, scale=0.01).rvs(size=1000)
+b = stats.norm(loc=0.24, scale=0.02).rvs(size=1000)
+fig, ax = plt.subplots()
+sns.kdeplot(a, shade=True, ax=ax)
+sns.kdeplot(b, shade=True, ax=ax)
+
+#%%
+deltas = a - b
+sns.kdeplot(np.random.choice(deltas, size=(2000, 1000)).mean(axis=1))
+
+#%%
+new_a = np.random.choice(a, size=(2000, 1000)).mean(axis=1)
+new_b = np.random.choice(b, size=(2000, 1000)).mean(axis=1)
+deltas = new_a - new_b
+sns.kdeplot(deltas)
