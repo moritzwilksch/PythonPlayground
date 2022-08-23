38066c84f28ad619f3d05973225621ad0bdff93d
Moritz Wilksch <moritzwilksch@gmail.com>
2022-08-23 14:33:44 +0000

    stuff

--- Statistics/motor_claims.py
+++ Statistics/motor_claims.py
@@ -1,5 +1,6 @@
 #%%
 from audioop import lin2adpcm
+from multiprocessing.connection import Pipe
 from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,15 +19,26 @@ plt.hist(df["Exposure"])
 
 #%%
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler
+from sklearn.pipeline import Pipeline
 
 transformer = ColumnTransformer(
     [
-        ("ohe", OneHotEncoder(sparse=False), ["Area", "VehGas", "Region", "VehBrand"]),
-        ("drop_useless", "drop", ["IDpol", "ClaimNb", "Exposure", "Frequency"]),
+        (
+            "ohe",
+            OneHotEncoder(sparse=False, drop="first"),
+            ["Area", "VehGas", "Region", "VehBrand", "VehPower"],
+        ),
+        ("drop_useless", "drop", ["IDpol", "ClaimNb", "Frequency"]),
+        (
+            "scale_numeric",
+            StandardScaler(),
+            ["Exposure", "DrivAge", "VehAge", "BonusMalus", "Density"],
+        ),
     ],
     remainder="passthrough",
 )
+
 
 X = transformer.fit_transform(df)
 y = df["Frequency"]
@@ -38,25 +50,93 @@ from sklearn.ensemble import HistGradientBoostingRegressor
 
 
 def evaluate(ytrue, preds):
-    print("MAE = ", mean_absolute_error(ytrue, preds))
-    print("MSE = ", mean_squared_error(ytrue, preds))
-    print("MPD = ", mean_poisson_deviance(ytrue, preds))
+    print("MAE = ", mean_absolute_error(ytrue, preds, sample_weight=df["Exposure"]))
+    print("MSE = ", mean_squared_error(ytrue, preds, sample_weight=df["Exposure"]))
+    print("MPD = ", mean_poisson_deviance(ytrue, preds, sample_weight=df["Exposure"]))
 
 
 models = [
     LinearRegression(),
-    PoissonRegressor(),
-    HistGradientBoostingRegressor(loss="poisson"),
+    PoissonRegressor(alpha=0),
+    HistGradientBoostingRegressor(),
 ]
 
 for model in models:
     print(f"{str(model.__class__):-^80}")
-    model.fit(X, y)
+    model.fit(X, y, sample_weight=df["Exposure"])
     preds = model.predict(X)
     preds[preds < 0] = 1e-9
     evaluate(y, preds)
 
 #%%
-df["Frequency"].hist(log=True)
+preds = model.predict(X)
+preddf = pd.DataFrame(
+    {
+        "true": y,
+        "pred": preds,
+        "preds*exp": preds * df["Exposure"],
+        "ClaimNb": df["ClaimNb"],
+    }
+)
+preddf
 
 #%%
+preddf.assign(true=lambda d: d["true"].round()).groupby("true")["pred"].mean()
+
+#%%
+
+#%%
+plt.scatter(y, preds)
+
+#%%
+df.query("Frequency == 0")
+
+#%%
+np.exp(X @ model.coef_ + model.intercept_)
+
+#%%
+model.predict(X)
+
+#%%
+import seaborn as sns
+
+tips = sns.load_dataset("tips")
+X = tips.drop("tip", axis=1)
+y = tips["tip"]
+y = np.log(y)
+# X = X.assign(total_bill=np.log(X["total_bill"]))
+
+transformer = ColumnTransformer(
+    [("ohe", OneHotEncoder(), tips.select_dtypes("category").columns)],
+    remainder="passthrough",
+)
+
+X = transformer.fit_transform(X)
+
+# model = GammaRegressor()
+model = LinearRegression()
+model = HistGradientBoostingRegressor()
+
+model.fit(X, y)
+preds = model.predict(X)
+
+print(mean_squared_error(y, preds))
+print(mean_absolute_error(y, preds))
+
+#%%
+pd.DataFrame({"true": y, "pred": preds})
+
+#%%
+plt.scatter(y, preds - y)
+
+#%%
+fig, axes = plt.subplots(ncols=2, figsize=(10, 5))
+axes[0].scatter(y, preds)
+axes[1].hist(y - preds)
+
+#%%
+from scipy import stats
+
+x = np.arange(0, 10, 0.01)
+y = stats.gamma(a=0.5, scale=1).pdf(x)
+plt.plot(x, y)
