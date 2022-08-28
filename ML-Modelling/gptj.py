6cb54fa8609d6a60732ecb2a19390e9bdcc1e347
Moritz Wilksch <moritzwilksch@gmail.com>
2022-08-28 09:09:03 +0200

    gpt-j

--- ML-Modelling/gptj.py
+++ ML-Modelling/gptj.py
@@ -6,24 +6,6 @@ pipe = pipeline("text-generation", model="EleutherAI/gpt-j-6B")
 
 #%%
 pipe(
-    """
-    Generate domain names for a technology startup.
-    domain: www.zwei.dev
-    domain: www.testr.io
-    domain: www.
-    """,
+    "",
     max_new_tokens=100,
 )
-
-#%%
-from sklearn.datasets import fetch_20newsgroups
-import pandas as pd
-
-df = fetch_20newsgroups(
-    subset="train", categories=["sci.med"], shuffle=True, random_state=42
-)
-
-df = pd.DataFrame(df)
-
-
-
