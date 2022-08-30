46355e848b1d4e1ca10591e35a4abbe21ba6734d
Moritz Wilksch <moritzwilksch@gmail.com>
2022-08-30 12:54:04 +0200

    pymc toy examples

--- Statistics/new_pymc.py
+++ Statistics/new_pymc.py
@@ -1,13 +1,14 @@
 #%%
-import aesara.tensor as at
+from curses import nl
+from email.errors import ObsoleteHeaderDefect
 import arviz as az
 import matplotlib.pyplot as plt
 import numpy as np
 import pymc as pm
 
 #%%
-data = [0]* 64 + [1]*16
-data_2 = [0]* 59 + [1]*21
+data = [0] * 64 + [1] * 16
+data_2 = [0] * 59 + [1] * 21
 
 with pm.Model() as model:
     ctr = pm.Beta("beta", alpha=1, beta=1)
@@ -37,4 +38,52 @@ plt.plot(x, y)
 az.plot_posterior(sampeled)
 
 #%%
-(sampeled.to_dataframe()[('posterior', 'beta')] < sampeled_2.to_dataframe()[('posterior', 'beta')]).mean()
+(
+    sampeled.to_dataframe()[("posterior", "beta")]
+    < sampeled_2.to_dataframe()[("posterior", "beta")]
+).mean()
+
+#%%
+# Locomotive Problem
+from aesara import tensor as at
+
+
+with pm.Model() as model:
+    OBSERVED = 60
+    n_lok = pm.Gamma("n_lok", alpha=1, beta=10)
+
+    # likelihood = pm.Censored("censored_lik", pm.DiscreteUniform.dist(1, n_lok), lower=OBSERVED, upper=None)
+    likelihood = pm.DiscreteUniform(
+        "lik", 1, pm.Deterministic("n_lok/200", n_lok * 200 + 60), observed=OBSERVED
+    )
+
+    sampeled = pm.sample(chains=6)
+
+az.plot_posterior(sampeled)
+
+
+#%%
+sampeled.to_dataframe()[("posterior", "n_lok")].mean()
+
+#%%
+sampeled.to_dataframe()
+#%%
+from scipy import stats
+
+x = np.arange(0, 1000)
+y = stats.expon().pdf((x - 60) / 200)
+plt.plot(x, y)
+
+
+#%%
+y2 = stats.gamma(a=1).pdf(x / 200)
+plt.plot(x, y2, color="red")
+
+#%%
+with pm.Model() as m:
+    gamma = pm.Deterministic("n_lok/200", pm.Gamma("gamma", alpha=1, beta=1) * 200 + 60)
+    res = pm.sample()
+az.plot_posterior(res)
+
+#%%
+sampeled.__dir__()
