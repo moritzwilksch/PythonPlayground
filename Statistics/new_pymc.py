02e38d623cdb0347f8527e248a9a8fb81634c933
Moritz Wilksch <moritzwilksch@gmail.com>
2022-09-01 08:21:22 +0200

    function optimization

--- Statistics/new_pymc.py
+++ Statistics/new_pymc.py
@@ -1,10 +1,9 @@
 #%%
-from curses import nl
-from email.errors import ObsoleteHeaderDefect
 import arviz as az
 import matplotlib.pyplot as plt
 import numpy as np
 import pymc as pm
+import pandas as pd
 
 #%%
 data = [0] * 64 + [1] * 16
@@ -53,9 +52,7 @@ with pm.Model() as model:
     n_lok = pm.Gamma("n_lok", alpha=1, beta=10)
 
     # likelihood = pm.Censored("censored_lik", pm.DiscreteUniform.dist(1, n_lok), lower=OBSERVED, upper=None)
-    likelihood = pm.DiscreteUniform(
-        "lik", 1, pm.Deterministic("n_lok/200", n_lok * 200 + 60), observed=OBSERVED
-    )
+    likelihood = pm.DiscreteUniform("lik", 1, n_lok, observed=OBSERVED)
 
     sampeled = pm.sample(chains=6)
 
@@ -70,8 +67,8 @@ sampeled.to_dataframe()
 #%%
 from scipy import stats
 
-x = np.arange(0, 1000)
-y = stats.expon().pdf((x - 60) / 200)
+x = np.arange(0, 1000, 0.1)
+y = stats.weibull_min(1, loc=60, scale=2).pdf(x)
 plt.plot(x, y)
 
 
@@ -86,4 +83,43 @@ with pm.Model() as m:
 az.plot_posterior(res)
 
 #%%
-sampeled.__dir__()
+with pm.Model() as m:
+    sex = pm.Categorical("sex", [0.4, 0.6])
+
+    if sex == 1:
+        age_group = pm.Categorical("age_group", [0.4, 0.4, 0.2])
+    else:
+        age_group = pm.Categorical("age_group", [0.33, 0.33, 0.33])
+    s = pm.sample(chains=1, draws=48)
+
+#%%
+data = s.to_dataframe()
+pd.crosstab(data["sex"], data["age_group"])
+
+
+#%%
+@np.vectorize
+def additional_profit_generated(price, cost, units_sold, inc_vol, pct_price_change):
+    new_units_sold = units_sold * (1 + inc_vol * -pct_price_change / 0.1)
+    print(f"{units_sold =}, {new_units_sold =}")
+
+    old_profit = price - cost
+    new_profit = price * (1 + pct_price_change) - cost
+    print(f"{old_profit = }, {new_profit = }")
+    return new_profit * new_units_sold - old_profit * units_sold
+
+
+changes = np.arange(-0.1, 0.2, 0.01)
+y = additional_profit_generated(
+    price=110, cost=100, units_sold=50, inc_vol=0.2, pct_price_change=changes
+)
+plt.plot(changes, y)
+
+#%%
+import scipy
+def criterion(delta):
+    return -1 * additional_profit_generated(
+        price=110, cost=100, units_sold=50, inc_vol=0.2, pct_price_change=delta
+    )   
+
+scipy.optimize.minimize(criterion, bounds=scipy.optimize.Bounds(-0.1, 0.2), x0=0)
